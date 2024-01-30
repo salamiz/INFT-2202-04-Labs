1e789728402820b53e7d52525a83e046a042d2b0
loganspagnolo <48230054+loganspagnolo@users.noreply.github.com>
2024-01-30 10:26:31 -0500

    Logan's First Commit
    -Moved files around to solve build errors
    -Had to install modules again (not sure if they will duplicate)
    - Added navbar to App.js

---
+++ lab1/src/App.js
@@ -0,0 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">INFT 2202 - Lab 1</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#products">Products</Nav.Link>
+              <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link href="#about">About Us</Nav.Link>
+              <Nav.Link href="#contact">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
